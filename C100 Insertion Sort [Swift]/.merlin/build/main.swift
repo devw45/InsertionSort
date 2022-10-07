24c86093ef7357e39962e9adf65d3d50e6d56f72
devin wingfield <devin.wingfield@student.allenisd.org>
2022-10-07 12:52:09 -0500

    txt files

---
+++ C100 Insertion Sort [Swift]/.merlin/build/main.swift
@@ -0,0 +1,31 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [-111, -149, 185, 683, 471, 202, -373, 255, 990, -992]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+// Add your code below:
+var swap = 0
+
+let length = unsortedIntegers.count
+var arr = unsortedIntegers
+
+for index in 0 ..< length  {
+    var pass = 0
+    let current = arr[index]
+    var previous = index - 1
+
+    while (previous >= 0 && current < arr[previous]) {
+        arr[previous + 1]  = arr[previous]
+        arr[previous] = current
+        swap += 1
+        pass += 1
+        previous -= 1
+    }
+    
+    print("Pass: \(index), Swaps: \(pass)/\(swap), Array: \(arr)")
+}
+
+
